11b2527fd61769c64bc78f7badc44d07e38517ed
Gabriella <bettontegabriella@gmail.com>
2018-04-07 18:07:20 +0200

    alternativ

--- src/main/java/it/unipd/dei/bdc1718/SecondHomeworkGabriella.java
+++ src/main/java/it/unipd/dei/bdc1718/SecondHomeworkGabriella.java
@@ -25,7 +25,7 @@ public class SecondHomeworkGabriella {
         Logger.getLogger("akka").setLevel(Level.OFF);
         SparkConf configuration =
                 new SparkConf(true)
-                        .setAppName("Second homework");
+                        .setAppName("Second homework ");
         JavaSparkContext sc = new JavaSparkContext(configuration);
 
         JavaRDD<String> docs = sc.textFile("text-sample.txt").cache();
@@ -33,46 +33,54 @@ public class SecondHomeworkGabriella {
         System.out.println(counter);
         int number_partition = 16;
         int t = (int) Math.floor(counter / number_partition); // numero di elementi per ogni partizione
-        String beppe = null;
         Scanner s = new Scanner(new FileReader("text-sample.txt"));
         ArrayList<String> subdocument = new ArrayList();
         String str;
         for (int j = 0; j < number_partition; j++) {
-            for (int k = 0; k < t; k++) {
-                {
-                    str = s.nextLine();
 
-                    //System.out.print("hhhgf");
-                    //System.out.print(str);
-                    subdocument.add(str);
-                    System.out.println(subdocument.size());
+            for (int k = 0; k < t; k++)
+            {
+                str = s.nextLine();
 
-                }
-                JavaRDD<String> dsubdocuments = sc.parallelize(subdocument);
+                subdocument.add(str);
+                System.out.println(subdocument.size());
 
-                JavaPairRDD<String, Long> alternativo = dsubdocuments
-                        .flatMapToPair((x) -> {             // <-- Map phase
-                            String[] tokens = x.split(" ");
-                            ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
-
-                            for (String token : tokens) {
-                                pairs.add(new Tuple2<>(token, 1L));
-
-                            }
-                            return pairs.iterator();
-
-
-                        }).groupByKey().mapValues((it) -> {
-                            long sum = 0;
-                            for (long c : it) {
-                                sum += c;
-                            }
-                            return sum;
-                        });
-                for (Tuple2 line : alternativo.collect()) {
-                    System.out.println("*" + line);
-                }
             }
+
+            JavaRDD<String> dsubdocuments = sc.parallelize(subdocument);
+            JavaPairRDD<String, Long> alternativo = dsubdocuments
+                    .flatMapToPair((x) -> {             // <-- Map phase
+                        String[] tokens = x.split(" ");
+                        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+
+                        for (String token : tokens) {
+                            pairs.add(new Tuple2<>(token, 1L));
+
+                        }
+                        return pairs.iterator();
+
+                    }).groupByKey().mapValues((it) -> {
+                        long sum = 0;
+                        for (long c : it) {
+                            sum += c;
+                        }
+                        return sum;
+                    });
+            PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
+            for (Tuple2 line : alternativo.collect()) {
+
+                System.out.println("*" + line);
+                out.println(line);
+
+            }
+            //create an print writer for writing to a file
+
+
+            //output to the file a line
+
+
+            //close the file (VERY IMPORTANT!)
+            out.close();
         }
     }
 }
