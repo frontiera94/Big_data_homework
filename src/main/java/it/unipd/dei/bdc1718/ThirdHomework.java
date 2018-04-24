6e0c560135c6aee19413a021683161a45666103f
MattiaPerini <perinimattia94@gmail.com>
2018-04-24 17:53:47 +0200

    elena  <3

--- src/main/java/it/unipd/dei/bdc1718/ThirdHomework.java
+++ src/main/java/it/unipd/dei/bdc1718/ThirdHomework.java
@@ -54,27 +54,37 @@ public class ThirdHomework
 
     }*/
 
+    // KCENTER METHOD USING FARTHEST-FIRST TRAVERSAL ALGORITHM
 
+    // P = set of points
+    // k = number of centers
     public static  ArrayList<Vector> kcenter(ArrayList<Vector> P, int k)
     {
 
         //Double[] value=new Double[P.size()];
         //Arrays.fill(value,Integer.MAX_VALUE);
         //ArrayList<Double> c=new ArrayList<Double>(Arrays.asList(value));
+
+        // define the set P-S
         ArrayList<Tuple2<Vector,Double>> PS =new ArrayList<Tuple2<Vector,Double>>();
 
+        // initialize an arrayList of tuple of type<Point,distance> with distance = + infinity
         for(int i=0;i<P.size();i++)
         {
             Tuple2<Vector,Double> ne = new Tuple2<Vector,Double>( P.get(i),(double)Integer.MAX_VALUE);
             PS.add(ne);
         }
+        // we pick as first center an arbitrary point (for simplicity the first element of PS)
         ArrayList<Vector> S =new ArrayList<Vector>();
         S.add(PS.get(0)._1);
         PS.remove(0);
+
         for(int i=1;i<k;i++)
         {
+            // for each point c of P-S we compute the distance between the point and the last center added to S
             for(int j= 0; j < PS.size(); j++)
             {
+                // compute the minimum distance between a point c of P-S and the set S
                 Double dist=Vectors.sqdist(PS.get(j)._1(),S.get(S.size()-1));
                 if(dist<PS.get(j)._2)
                 {
@@ -83,38 +93,51 @@ public class ThirdHomework
                 }
             }
 
-            double max=PS.get(0)._2;
+            // find the point c of P-S that maximizes d(c,S)
+            double distmax=PS.get(0)._2;
             int i_max=0;
-           for (int m=0; m< PS.size();m++)
-           {
-               if(PS.get(m)._2>max)
+            for (int m=0; m < PS.size();m++)
+            {
+                if(PS.get(m)._2 > distmax)
                {
-                   max = PS.get(m)._2;
+                   distmax = PS.get(m)._2;
                    i_max = m;
                }
 
-           }
+            }
 
+            // add the point c to the set of centers S
             Tuple2<Vector, Double> temp2 = PS.get(i_max);
             S.add(temp2._1);
             PS.remove(temp2);
         }
 
+        // return the set of k centers
         return S;
     }
 
-    public static  ArrayList<Vector> kmeans(ArrayList<Vector> P, ArrayList<Double> WP, int k)
+    // KMEANSPP
+    //P = set of points
+    // WP = set of weight for P
+    // k = number of cluster
+    public static  ArrayList<Vector> kmeansPP(ArrayList<Vector> P, ArrayList<Double> WP, int k)
     {
+        // create an arrayList of Tuple3 of type<point,weight,distance from S>
         ArrayList<Tuple3<Vector,Double,Double>> PS =new ArrayList<Tuple3<Vector,Double,Double>>();
         for(int i=0;i<P.size();i++)
         {
+            // initialize all the distances equal to + infinity
             Tuple3<Vector,Double,Double> ne = new Tuple3<Vector,Double,Double>( P.get(i),WP.get(i),(double)Integer.MAX_VALUE);
             PS.add(ne);
         }
+
+        // choose a random point from P with uniform probability
+        Random ran = new Random();
+        int i_chosen = ran.nextInt(P.size());
         ArrayList<Vector> S =new ArrayList<Vector>();
         ArrayList<Double> prob = new ArrayList<>(P.size());
-        S.add(PS.get(0)._1());
-        PS.remove(0);
+        S.add(PS.get(i_chosen)._1());
+        PS.remove(i_chosen);
 
         for(int i=1;i<k;i++)
         {
@@ -172,6 +195,24 @@ public class ThirdHomework
     }
 
 
+    public static  ArrayList<Tuple2<Vector,Double>> kmeansObj(ArrayList<Vector> P, ArrayList<Vector> C)
+    {
+        ArrayList<Tuple2<Vector,Double>> end = new ArrayList<>();
+        for(int i=0;i<P.size();i++)
+        {
+            double dist = 0;
+            double mindist = Double.MAX_VALUE;
+            for(int j=0;j<C.size();j++)
+            {
+                dist =  Vectors.sqdist(P.get(i),C.get(j))/P.size();
+                if(dist < mindist)
+                    mindist = dist;
+            }
+            Tuple2<Vector,Double> temp = new Tuple2<>(P.get(i),mindist);
+            end.add(temp);
+        }
+        return end;
+    }
 
 
 
